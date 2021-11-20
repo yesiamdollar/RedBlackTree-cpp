b6e7ebab8258576dc75bc34d3de846938f54b0df
yesiamdollar <achrafboutahri17@gmail.com>
2021-11-20 20:03:38 +0100

    start with insert and set the root

---
+++ RedBlackTree.hpp
@@ -0,0 +1,45 @@
+#ifndef RED_BLACK_TREE_HPP
+
+# define RED_BLACK_TREE_HPP
+
+
+# include <iostream>
+
+# define	RED	1
+# define	BLACK	0
+
+struct Node{
+	int	key;
+	Node*	parent;
+	Node*	left;
+	Node*	right;
+	int	color;
+}
+
+typedef Node*	Nodeptr;
+
+class	RBTree{
+	private:
+		Nodeptr	root;
+
+
+	public:
+		RBTree(): root(0) {}
+
+		bool insert(int key){
+			Nodeptr	ptr = new Node;
+			ptr->key = key;
+			ptr->parent = nullptr;
+			ptr->left = nullptr;
+			ptr->left = nullptr;
+			ptr->color = RED;
+
+			if (root == 0){
+				root = ptr;
+				root->color = BLACK;
+				return true;
+			}
+		}
+}
+
+#endif
