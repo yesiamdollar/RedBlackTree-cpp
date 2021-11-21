48f087abcb9342591d3eb5584f970637d7ae6806
Achraf Boutahri <aboutahr@e2r1p7.1337.ma>
2021-11-21 15:59:29 +0100

    working on fixRBT

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -9,6 +9,9 @@
 # define	BLACK	0
 
 struct Node{
+	Node(int k) : key(k), parent(nullptr), left(nullptr), right(nullptr), color(RED){
+
+	};
 	int				key;
 	Node*			parent;
 	Node*			left;
@@ -21,26 +24,43 @@ typedef Node*	Nodeptr;
 class	RBTree{
 	private:
 		Nodeptr	root;
+		int		blacks;
+		Nodeptr	RBTinsert(Nodeptr src, Nodeptr ptr){
+			if (src == nullptr){
+				return ptr;
+			}
+			if (ptr->key < src->key){
+				src->left = RBTinsert(src->left, ptr);
+				src->left->parent = src;
+			} else {
+				src->right = RBTinsert(src->right, ptr);
+				src->right->parent = src;
+			}
+			return src;
+		}
 
+		void	fixRBT(Nodeptr&	ptr){
+			Nodeptr	parent, grandparent, uncle;
+
+			if (ptr == root){
+				root->color = BLACK;
+				blacks++;
+				return ;
+			}
+			parent = ptr->parent;
+			grandparent = parent->parent;
+			if (parent->color == BLACK)
+				return ;
+
+		}
 
 	public:
-		RBTree(): root(0) {}
+		RBTree(): root(nullptr), blacks(0) {}
 
-		bool insert(int key){
-			Nodeptr	ptr = new Node;
-			ptr->key = key;
-			ptr->parent = nullptr;
-			ptr->left = nullptr;
-			ptr->left = nullptr;
-			ptr->color = RED;
-			if (root == 0){
-				root = ptr;
-				root->color = BLACK;
-				return true;
-			}
-			Nodeptr	current = root;
-			
-			return false;
+		void insert(int key){
+			Nodeptr	ptr = new Node(key);
+			root = RBTinsert(root, ptr);
+			fixRBT(ptr);
 		}
 };
 
