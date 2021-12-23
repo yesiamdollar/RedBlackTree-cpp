dcea392dc5a9f8ee8b38ab3e66d32d13ee16739b
yesiamdollar <achrafboutahri17@gmail.com>
2021-12-23 16:25:17 +0100

    delete ✅

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 int	main(){
 	// ft::pair<int, int> p1 = ft::make_pair(0, 1);
 	ft::RBTree<int> tree;
-	// int a[9] = {55, 40, 65, 60, 75, 57, 56, 64, 12};
-	for (int i = 0; i < 9; i++)
+	int a[9] = {55, 40, 65, 60, 75, 57, 56, 64, 12};
+	for (int i = 0; i < 10; i++)
 		tree.insert(i);
 	
 	tree.printTree();
-	tree.remove(12);
+	std::cout << "----------------------------------------\n";
+	// tree.remove(8);
+	// tree.printTree();
+	std::cout << "----------------------------------------\n";
+	tree.remove(3);
 	tree.printTree();
 	// ft::RBTree<int>::Nodeptr n = tree.search(30);
 	// if (n)
